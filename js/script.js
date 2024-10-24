2a6d71dca435855c3a8056f5ced61b502c666d06
leminhhoang1001 <32322377+leminhhoang1001@users.noreply.github.com>
2024-10-24 23:57:20 +0700

    update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,21 @@ const tapHint = document.getElementById('tap-hint')
 const button = document.getElementById('muteaudio');
 const musicOn = '<i class="fas fa-volume-high"></i>';
 const musicOff = '<i class="fas fa-volume-xmark"></i>';
-
+const messagelist = [
+    "Count your life by smiles, not tears. Count your age by friends, not years.",
+    "I hope all your birthday wishes and dreams come true.",
+    "A wish for you on your birthday, whatever you ask may you receive, whatever you seek may you find, whatever you wish may it be fulfilled on your birthday and always.",
+    "May the joy that you have spread in the past come back to you on this day.",
+    "Your life is just about to pick up speed and blast off into the stratosphere. Wear a seat belt and be sure to enjoy the journey.",
+    "Forget the past; look forward to the future, for the best things are yet to come.",
+    "You’re older today than yesterday but younger than tomorrow!",
+    "Don’t get all weird about getting older! Our age is merely the number of years the world has been enjoying us!",
+    "You are only young once, but you can be immature for a lifetime."
+];
+const random = Math.floor(Math.random() * messagelist.length);
+var i = 0;
+var speed = 50;
+var message = messagelist[random];
 const muteSound = new Howl({
     src: ['./audio/Romantic-Happy-Birthday.mp3'],
     // mute: false,
@@ -48,3 +62,15 @@ button.addEventListener("click", () => {
     // toggle the muted property of the audio element
     muteSound.muted = !muteSound.muted;
 });
+
+// Random TypeWriter Message
+
+card.addEventListener('click', function() {
+    setTimeout(function() {
+        var typed = new Typed('.greeting-content', {
+            strings: [messagelist[random]],
+            typeSpeed: 40,
+            showCursor: false
+        });
+    }, 2000);
+}, { once: true });
